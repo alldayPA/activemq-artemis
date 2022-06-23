ffdcbf30d838efcd68b0a81d72109941a41250cd
mike.pickup <mike.pickup@alldaypa.com>
2022-06-23 09:46:46 +0100

    use archive cdn for download

--- artemis-docker/prepare-docker.sh
+++ artemis-docker/prepare-docker.sh
@@ -124,7 +124,7 @@ if [ -n "${FROM_RELEASE}" ]; then
 
   cleanup
   
-  ARTEMIS_BASE_URL="$(curl -s https://www.apache.org/dyn/closer.cgi\?preferred=true)activemq/activemq-artemis/${ARTEMIS_VERSION}/"
+  ARTEMIS_BASE_URL="https://archive.apache.org/dist/activemq/activemq-artemis/${ARTEMIS_VERSION}/"
   ARTEMIS_DIST_FILE_NAME="apache-artemis-${ARTEMIS_VERSION}-bin.tar.gz"
   CURL_OUTPUT="${BASE_TMPDIR}/${ARTEMIS_VERSION}/${ARTEMIS_DIST_FILE_NAME}"
 
